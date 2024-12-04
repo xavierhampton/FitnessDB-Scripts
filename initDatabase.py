ad0089f55f1c9853308bd67fca5d4f75a8f69ae6
Xavier Hampton <xaviermichaelhampton@gmail.com>
2024-12-04 16:17:08 -0600

    more init

--- initDatabase.py
+++ initDatabase.py
@@ -1,5 +1,21 @@
 import mysql.connector
 
+#Connects to database and creates cursor
 cnx = mysql.connector.connect(user='root', password='65821',
                               host='127.0.0.1')
+cursor = cnx.cursor()
+#Resets by dropping the database "fitness"
+try:
+    cursor.execute("DROP DATABASE FITNESS")
+except:
+    print('No Database to Drop. Continuing\n')
+
+cursor.execute("CREATE DATABASE FITNESS")
+cursor.execute("USE FITNESS")
+
+cursor.execute("""CREATE TABLE TRAINER (
+                )""")
+
+
+#Closes the connection when done
 cnx.close()
