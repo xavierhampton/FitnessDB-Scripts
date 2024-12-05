a2d5f4d0da27e83e9fad67ce3c81fc91b907fd83
Xavier Hampton <xaviermichaelhampton@gmail.com>
2024-12-05 01:09:11 -0600

    finished inserting test data in init file

--- initDatabase.py
+++ initDatabase.py
@@ -13,6 +13,8 @@ except:
 cursor.execute("CREATE DATABASE FITNESS")
 cursor.execute("USE FITNESS")
 
+#---------------------------------------------------
+
 #Creates Trainer Table
 cursor.execute("""CREATE TABLE TRAINER (
                 Trainer_ID          INT             NOT NULL,
@@ -64,23 +66,100 @@ cursor.execute("""CREATE TABLE APPOINTMENT (
 
 #Creates Exercise Table
 cursor.execute("""CREATE TABLE EXERCISE (
-                Exercise_ID         INT             NOT NULL,
-                Client              INT,
-                Date                DATE,
-                Name                VARCHAR(15),
+                Exercise_ID         INT             NOT NULL        AUTO_INCREMENT,
+                Client              INT             NOT NULL,
+                Date                DATE            NOT NULL,
+                Name                VARCHAR(30),
                 Weight              INT,
                 Rep_Count           INT,
-                PRIMARY KEY(Exercise_ID),
+                PRIMARY KEY(Exercise_ID, Client),
                 FOREIGN KEY(Client) REFERENCES CLIENT(Client_ID)
                )""")
 
 #Creates Language Table
 cursor.execute("""CREATE TABLE TRAINER_LANGUAGES (
                 Trainer_ID          INT             NOT NULL,
-                Language            VARCHAR(20),
+                Language            VARCHAR(20)     NOT NULL,
+                PRIMARY KEY(Trainer_ID, Language),
                 FOREIGN KEY(Trainer_ID) REFERENCES TRAINER(Trainer_ID)
                )""")
 
 
+#------------------------------------------------------
+
+#Creates Synthetic Data
+
+#Creates Trainer Data
+cursor.execute("""INSERT INTO TRAINER 
+               VALUES (111, 'John', 'Gates', 'M', 10, '2000-10-14'),
+                      (222, 'Bob', 'Apple', 'M', 2, '1994-01-20'),
+                      (333, 'Sydney', 'Cheese', 'F', 6, '1995-08-31')
+               """)
+
+#Creates Client Data
+cursor.execute("""INSERT INTO CLIENT 
+               VALUES (100, 'Xavier', 'Hampton', 'M', 68, 170, '2005-02-03', 111),
+                      (200, 'George', 'Washington', 'M', 64, 190, '1842-04-14', 111),
+                      (300, 'Janice', 'Quart', 'F', 59, 120, '1997-04-23', 333),
+                      (400, 'Jimmy', 'Brown', 'M', 73, 205, '2001-02-27', 222),
+                      (500, 'Lebron', 'James', 'M', 78, 185, '1987-07-05', 222)
+               """)
+
+#Creates Gym Data
+cursor.execute("""INSERT INTO GYM 
+               VALUES (10, 'Golds Gym', 'Imperial', 'MO', 63052, '200 Oxford Lane'),
+                      (20, 'Planet Fitness', 'Arnold', 'MO', 63051, '320 Smiley Lane'),
+                      (30, 'Life Fitness', 'Springfield', 'MO', 67201, '100 Street Lane')
+               """)
+
+#Creates Appointment Data
+cursor.execute("""INSERT INTO APPOINTMENT 
+               VALUES (11, '2024-12-01', '11:30:00', 111, 100, 30),
+                      (22, '2024-12-25', '16:00:00', 111, 200, 20),
+                      (33, '2025-01-10', '14:45:00', 111, 200, 10),
+                      (44, '2025-02-03', '14:30:00', 222, 300, 10),
+                      (55, '2024-11-20', '12:45:00', 222, 400, 30),
+                      (66, '2025-01-02', '18:00:00', 222, 300, 20),
+                      (77, '2024-12-26', '16:00:00', 333, 300, 10),
+                      (88, '2024-10-31', '15:30:00', 333, 300, 30),
+                      (99, '2025-01-01', '16:45:00', 333, 300, 20)
+               """)
+
+#Creates Exercise Data
+cursor.execute("""INSERT INTO EXERCISE (Client, Date, Name, Weight, Rep_Count) 
+               VALUES (100, '2024-12-10', 'Dumbbell Curls', 30, 12),
+                      (100, '2024-12-10', 'Cable Tricep Pushdowns', 70, 8),
+                      (100, '2024-12-10', 'Dumbbell Shoulder Press', 60, 10),
+                      (200, '2024-12-10', 'Leg Press', 30, 5),
+                      (200, '2024-12-10', 'Seated Hamstring Curls', 35, 12),
+                      (200, '2024-12-10', 'Calf Raises', 75, 8),
+                      (300, '2024-12-10', 'Chest Press', 15, 10),
+                      (300, '2024-12-10', 'T-Bar Rows', 25, 9),
+                      (300, '2024-12-10', 'Lateral Pulldowns', 100, 6),
+                      (400, '2024-12-10', 'Barbell Squat', 90, 12),
+                      (400, '2024-12-10', 'Spider Curls', 35, 12),
+                      (400, '2024-12-10', 'Reverse Curls', 25, 8),
+                      (400, '2024-12-10', 'Deadlift', 20, 9),
+                      (500, '2024-12-10', 'Skull Crushers', 70, 7),
+                      (500, '2024-12-10', 'Power clean', 80, 15),
+                      (500, '2024-12-10', 'Low Rows', 65, 8),
+                      (500, '2024-12-10', 'Pull-Ups', 60, 12),
+                      (500, '2024-12-10', 'Dips', 5, 6)
+               """)
+
+#Creates Trainer Languages Data
+cursor.execute("""INSERT INTO TRAINER_LANGUAGES
+               VALUES (111, 'English'),
+                      (111, 'Spanish'),
+                      (222, 'English'),
+                      (333, 'English')
+               """)
+
+
+
+#------------------------------------------------------
 #Closes the connection when done
+cnx.commit()
+
+cursor.close()
 cnx.close()
